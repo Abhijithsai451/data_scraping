f7e0eadc29df45a83db59ba1125edaea81abdbd0
Abhijith Sai Thirunahari <>
2023-01-09 15:19:14 -0500

    initial commit

--- weather_data_scraper.py
+++ weather_data_scraper.py
@@ -1,28 +1,23 @@
-import datetime
-
-import requests
-import os
-from datetime import date
+import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
 
-
+import project_secrets
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://severeweather.wmo.int/v2/list.html")
+driver.get(project_secrets.weather_data_url)
 
 try:
-    elem = WebDriverWait(driver,30).until(
-        EC.presence_of_element_located((By.CLASS_NAME,"dataTables_scrollBody")))
+    elem = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "dataTables_scrollBody")))
 finally:
     print('loaded')
 
-soup = BeautifulSoup(driver.page_source,'html.parser')
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 # Exracting the data from the html page
 all = soup.findAll('tbody')[2]
 rows = all.findAll('tr')
@@ -31,7 +26,7 @@ rows = all.findAll('tr')
 data = []
 for i in rows:
     infos_row = i.findAll('td')
-    for index,j in enumerate(infos_row):
+    for index, j in enumerate(infos_row):
         info = None
         if index == 0:
             info = j.find('span')
@@ -47,9 +42,8 @@ for i in rows:
             regions = j.text
         if index == 2:
             continue
-    data.append([event,issued_time,country,areas,regions])
+    data.append([event, issued_time, country, areas, regions])
 
 # Saving the data as a Pandas data frame and then to convert it as a CSV
-df = pd.DataFrame(data, columns=['Event_type','Issued_time','Country','Areas','Regions'])
-df.to_csv("scrapped_weather.csv",mode='a',index = False, header = False)
-
+df = pd.DataFrame(data, columns=['Event_type', 'Issued_time', 'Country', 'Areas', 'Regions'])
+df.to_csv("scrapped_weather.csv", mode='a', index=False, header=False)
